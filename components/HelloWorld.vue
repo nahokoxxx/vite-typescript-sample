6c23ec3eaafa08241e109648b13fb93aae923fa9
Nahoko Matsui <nahokomatsui8213@gmail.com>
2020-05-19 17:44:41 +0900

    setup vite project with typescript

---
+++ components/HelloWorld.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h1>Hello Vue 3!</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue'
+
+export default defineComponent({})
+</script>
